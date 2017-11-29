94f3fc933b7c4867cf81465a35296156b6e22c0b
fresh5447 <doug@kosmojo.com>
2017-11-29 13:38:36 -0700

    get gift by id example

--- client/src/components/pages/admin/GiftPage.js
+++ client/src/components/pages/admin/GiftPage.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {Component} from 'react'
 import injectSheet from 'react-jss'
-import GiftCard from './gift/GiftCard'
-import {Route, withRouter} from 'react-router-dom'
+import {compose} from 'recompose'
+import {withRouter} from 'react-router-dom'
 import * as AppPropTypes from '../../../lib/propTypes'
 import PropTypes from 'prop-types'
 
@@ -59,28 +59,56 @@ const styles = {
   }
 }
 
-const enhancer = injectSheet(styles)
+// 1) this component needs to be smart so we can lookup a gift by it's ID
+// 2) You should probably call this `GiftPageContainer` and actually pass
+// the gift to a dumb GiftPage
+// 3) We  get the ID out of the url with the help of withRouter
+// 4) we use `compose` to enhance a component with multiple things.
+// IE styles and withRouter, though once this one becomes dumb, it will only
+// need withRotuter
 
-const GiftPage = (props, giftData) => {
-  return (
-    <div>
-      <div className={props.classes.mainDiv}>
-        <h1 className={props.classes.heroTitle}> Spread Holiday Cheer </h1>
-        <h3 className={props.classes.heroSubTitle}> Spread the Cheer by buying some Pajamas </h3>
+class GiftPage extends Component {
+  componentDidMount () {
+    console.log(this.props.match.params.giftId) // extract the ID out of the url
+    // we then use the ID to to look up the gift. 
+    // We can only use Match because we used withRouter
+    this.props.giftData.getGiftById(this.props.match.params.giftId)
+    // ^ Now we have a gift!
+  }
+
+  render () {
+    return (
+      <div>
+        <div className={this.props.classes.mainDiv}>
+          <h1 className={this.props.classes.heroTitle}> Spread Holiday Cheer </h1>
+          <h3 className={this.props.classes.heroSubTitle}> Spread the Cheer by buying some Pajamas </h3>
+        </div>
+        <div className={this.props.classes.giftCardContainer} >
+          {
+            this.props.giftData.gift
+              ? <h4> {this.props.giftData.gift.story}</h4>
+              : <p>Error Getting Gift</p>
+          }
+        </div>
       </div>
-      <div className={props.classes.giftCardContainer} >
-        Pizza
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 GiftPage.propTypes = {
   classes: PropTypes.object.isRequired,
   gift: AppPropTypes.gift,
-  giftData: AppPropTypes.giftData,
+  giftData: AppPropTypes.giftData
 }
 
-GiftPage.propTypes = propTypes
+// This componenet needs to be enhanced by more than
+// one thing, so we use compose.
+// enhance it with injectSheet and then withRouter
+// withRouter lets us use this.props.params
 
-export default enhancer(withRouter(GiftPage))
+const enhancer = compose(
+  injectSheet(styles),
+  withRouter
+)
+
+export default enhancer(GiftPage)
